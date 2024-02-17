bd03c264fe2e36562fde2a79882fb3747b6ee166
Influenzo <52459025+vasylcode@users.noreply.github.com>
2024-02-17 12:47:01 -0700

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Celestia Collector installation script.
+#
+# Usage:
+# . <(wget -qO- sh.f5nodes.com) celestia-collector -y
+
+echo "Select chain:"
+select chain in "mainnet" "mocha" "arabica"; do
+    case $chain in
+        mainnet)
+            endpoint="https://otel.celestia.observer"
+            echo "Chain: $chain"
+            break
+            ;;
+        mocha)
+            endpoint="https://otel.mocha.observer"
+            echo "Chain: $chain"
+            break
+            ;;
+        arabica)
+            endpoint="https://otel.arabica.observer"
+            echo "Chain: $chain"
+            break
+            ;;
+        *)
+            echo "Invalid option, please try again."
+            ;;
+    esac
+done
+
+echo "Select node type:"
+select node_type in "light" "full" "bridge"; do
+    case $node_type in
+        light|full|bridge)
+            echo "Node type: $node_type"
+            break
+            ;;
+        *)
+            echo "Invalid option, please try again."
+            ;;
+    esac
+done
+
+read -p "Enter nodename: " nodename
+echo "Nodename: $nodename"
+
+# Install OpenTelemetry
+
+wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.94.0/otelcol-contrib_0.94.0_linux_amd64.deb
+sudo dpkg -i otelcol-contrib_0.94.0_linux_amd64.deb
+rm otelcol-contrib_0.94.0_linux_amd64.deb
+otelcol-contrib --version
+
+sudo tee <<EOF >/dev/null /etc/otelcol-contrib/config.yaml
+receivers:
+  otlp:
+    protocols:
+      grpc: 
+      http:
+  prometheus:
+    config:
+      scrape_configs:
+      - job_name: 'otel-collector'
+        scrape_interval: 10s
+        static_configs:
+        - targets: ['127.0.0.1:8888']
+exporters:
+  otlphttp:
+    endpoint: $endpoint
+  prometheus:
+    endpoint: "0.0.0.0:8889"
+    namespace: celestia
+    send_timestamps: true
+    metric_expiration: 180m
+    enable_open_metrics: true
+    resource_to_telemetry_conversion:
+      enabled: true
+processors:
+  batch:
+  memory_limiter:
+    limit_mib: 1536
+    spike_limit_mib: 512
+    check_interval: 5s
+service:
+  pipelines:
+    metrics:
+      receivers: [otlp]
+      exporters: [otlphttp, prometheus]
+EOF
+
+systemctl daemon-reload
+systemctl restart otelcol-contrib
+
+# Install Telegraf
+
+wget https://dl.influxdata.com/telegraf/releases/telegraf_1.29.4-1_amd64.deb
+sudo dpkg -i telegraf_1.29.4-1_amd64.deb
+rm telegraf_1.29.4-1_amd64.deb
+telegraf --version
+
+cat <<EOF | sudo tee /etc/telegraf/telegraf.conf
+[agent]
+  hostname = "$nodename-$chain-$node_type"
+  interval = "15s"
+  flush_interval = "15s"
+  round_interval = true
+
+[[inputs.prometheus]]
+  urls = ["http://localhost:8889/metrics"]
+
+[[outputs.influxdb_v2]]
+  urls = ["https://celestia-metrics.f5nodes.com"]
+  token = "x-MJSrFAd3I7I3X2TWu9RAxw4TQYCRY7ArNrI3mGYar7nKU4ENp8DtJi1pqXCDcpYU2whBtlUenLesBw1rQKpQ=="
+  organization = "celestia-community"
+  bucket = "celestia-community-metrics"
+EOF
+
+sudo systemctl start telegraf
